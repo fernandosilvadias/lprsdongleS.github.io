58af717c9b31a2e7dfeaa45cefacdec1fca4ff6d
fernandosilvadias <47931141+fernandosilvadias@users.noreply.github.com>
2025-04-13 19:33:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,50 +20,11 @@ connectButton.onclick = async () => {
 
 
   
-  console.log(device.productName);      // "Arduino Micro"
-  console.log(device.manufacturerName); // "Arduino LLC"
   
-  await device.open();
-  await device.selectConfiguration(1);
-  await device.claimInterface(0);
 
-  connected.style.display = "block";
-  connectButton.style.display = "none";
-  disconnectButton.style.display = "initial";
-  listen();
-};
 
-const listen = async () => {
-  const result = await device.transferIn(3, 64);
 
-  const decoder = new TextDecoder();
-  const message = decoder.decode(result.data);
 
-  const messageParts = message.split(" = ");
-  if (messageParts[0] === "Count") {
-    deviceHeartbeat.innerText = messageParts[1];
-  } else if (messageParts[0] === "Button" && messageParts[1] === "1") {
-    deviceButtonPressed.innerText = new Date().toLocaleString("en-ZA", {
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
-    });
-  }
-  listen();
-};
-
-const hexToRgb = hex => {
-  const r = parseInt(hex.substring(1, 3), 16); //start at 1 to avoid #
-  const g = parseInt(hex.substring(3, 5), 16);
-  const b = parseInt(hex.substring(5, 7), 16);
-
-  return [r, g, b];
-};
-
-colourButton.onclick = async () => {
-  const data = new Uint8Array([1, ...hexToRgb(colourPicker.value)]);
-  await device.transferOut(2, data);
-};
 
 disconnectButton.onclick = async () => {
   await device.close();
