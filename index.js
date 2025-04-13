7867165585be813c7ae042e8ba493ddfd2729232
fernandosilvadias <47931141+fernandosilvadias@users.noreply.github.com>
2025-04-13 19:29:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ const deviceButtonPressed = document.getElementById("deviceButtonPressed");
 
 let device;
 connectButton.onclick = async () => {
-  device = await navigator.usb.requestDevice({
-    filters: [{ vendorId: 0x1A86 }]
-  });
+  device = navigator.usb.requestDevice({ filters: [{ vendorId: 0x1A86 }] })
+.then(device => {
+  console.log(device.productName);      // "Arduino Micro"
+  console.log(device.manufacturerName); // "Arduino LLC"
+})
+.catch(error => { console.error(error); });
 
+
+
+
+  
   console.log(device.productName);      // "Arduino Micro"
   console.log(device.manufacturerName); // "Arduino LLC"
   
